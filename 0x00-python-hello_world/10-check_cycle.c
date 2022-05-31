ef643fb817cec9a3ab8c783290dc109742b94b61
Kouam Franck <kouamfranck96@gmail.com>
2022-05-31 13:50:11 +0100

    Review Python Hello world tasks

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * check_cycle - check if a lincked list contains a cycle.
+ * @list: The linked list to check.
+ * Return: 1 if the list contains a cycle. 0 otherwise.
+ */
+int check_cycle(listint_t *list)
+{
+	return (0);
+}
