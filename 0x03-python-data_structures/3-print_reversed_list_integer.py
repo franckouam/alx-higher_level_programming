c5ee64189aab7b73a33d6f2195aed8a989b8f8aa
Kouam Franck <kouamfranck96@gmail.com>
2022-06-08 02:29:06 +0100

    Fixing some range issues

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    length = len(my_list)
-    for i in range(1, length+1):
-        print("{:d}".format(my_list[-i]))
+    if my_list:
+        length = len(my_list)
+        for i in range(1, length+1):
+            print("{:d}".format(my_list[-i]))
