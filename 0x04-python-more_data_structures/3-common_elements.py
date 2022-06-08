493b80c8d6225ef1a66d1276d619c8929f6a18c9
Kouam Franck <kouamfranck96@gmail.com>
2022-06-08 21:17:57 +0100

    0x04. Python - More Data Structures: Set, Dictionary | from 0 to 11

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if not set_1:
-        return
-    if not set_2:
-        return
+    if not set_1 or not set_2:
+        returin set()
     return set_1 & set_2
