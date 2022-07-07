6491a2e7d962726ab3b00e89afe901f399efea50
Kouam Franck <kouamfranck96@gmail.com>
2022-07-08 00:58:47 +0100

    MyList

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,9 +8,10 @@ MyList module
 class MyList(list):
     """A class that inherits from `list`"""
     def __init__(self, *args, **kwargs):
+        """Construct ``MyList`` instances."""
         super(list).__init__(*args, **kwargs)
 
     def print_sorted(self):
-        self
+        """Prints the sorted list(ascending sort)"""
         super(list).sort()
         print(self)
