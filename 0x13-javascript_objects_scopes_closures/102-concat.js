ff6742862cf7af3837bf72943afc759349a353da
Kouam Franck <kouamfranck96@gmail.com>
2022-09-07 14:53:38 +0100

    JavaScript objet and closures reviewing task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,10 +3,10 @@
 const fs = require('fs');
 const file1 = process.argv[2];
 const file2 = process.argv[3];
-const file_res = process.argv[4];
+const fileRes = process.argv[4];
 
 try {
   const data1 = fs.readFileSync(file1, 'utf-8');
   const data2 = fs.readFileSync(file2, 'utf-8');
-  fs.writeFile(file_res, data1 + data2, (err) => {});
-} catch (err) {}
+  fs.writeFile(fileRes, data1 + data2, (err) => { console.log(err); });
+} catch (err) { console.log(err); }
