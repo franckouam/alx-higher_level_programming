0d1f4cec4a301a782e43b026483e96ae8601f953
Kouam Franck <kouamfranck96@gmail.com>
2022-09-14 22:24:30 +0100

    0x0F. Python - Object-relational mapping | Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This script lists all ``states`` from a given database.
+"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    print(username, password, db_name)
+    db = MySQLdb.connect(host='127.0.0.1',
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=db_name)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
