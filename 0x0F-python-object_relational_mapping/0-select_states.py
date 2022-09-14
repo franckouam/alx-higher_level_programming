d37fe0ec1b3d07a90ea67a182ad6774e1fd5dd55
Kouam Franck <kouamfranck96@gmail.com>
2022-09-14 17:33:33 +0100

    0x0F. Python - Object-relational mapping | Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
+"""
+This script lists all ``states`` from a given database.
+"""
 import MySQLdb
 import sys
 
-if __name__=='__main__':
+if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
     print(username, password, db_name)
     db = MySQLdb.connect(host='127.0.0.1',
-		    port=3306,
-		    user=username,
-		    passwd=password,
-		    db=db_name)
+                         port=3306,
+                         user=username
+                         passwd=password,
+                         db=db_name)
     cur = db.cursor()
     cur.execute('SELECT * FROM states ORDER BY id ASC')
     rows = cur.fetchall()
