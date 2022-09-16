336f49335503ea145a37dd2112f86560b277382a
Kouam Franck <kouamfranck96@gmail.com>
2022-09-16 10:49:00 +0100

    0x0F. Python - Object-relational mapping | Tasks 0, 1, 2 review

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,6 @@ if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    print(username, password, db_name)
     db = MySQLdb.connect(host='127.0.0.1',
                          port=3306,
                          user=username,
