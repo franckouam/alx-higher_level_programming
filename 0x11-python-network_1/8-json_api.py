901a171283e4a99db57d170d5448efdb07c4ebe1
Kouam Franck <kouamfranck96@gmail.com>
2022-09-23 13:15:55 +0100

    0x11. Python - Network #1 | 8. Search API | Review

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,7 +8,7 @@ import sys
 
 if __name__ == '__main__':
     q = ""
-    if len0(sys.argv) > 1:
+    if len(sys.argv) > 1:
         q = sys.argv[1]
     url = "http://0.0.0.0:5000/search_user"
     data = {'q': q}
