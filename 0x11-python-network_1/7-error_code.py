9809bc1b05bf48d038c162057ca03ba79123a36b
Kouam Franck <kouamfranck96@gmail.com>
2022-09-23 12:31:21 +0100

    0x11. Python - Network #1 | 7. Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This script takes in a URL, sends a request to the URL and displays the
+   body of the response (decoded in utf-8).
+"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    try:
+        with requests.get(sys.argv[1]) as response:
+            print(response.text)
+    except requests.HTTPError as e:
+        print("Error code: {}".format(e.status_code))
