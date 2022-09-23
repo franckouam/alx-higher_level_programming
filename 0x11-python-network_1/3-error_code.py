49ab3d43f82f4d7a646549a5546deaa182a14790
Kouam Franck <kouamfranck96@gmail.com>
2022-09-23 12:02:48 +0100

    0x11. Python - Network #1 | 3. Error code #0 | Review

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,8 +9,8 @@ import sys
 if __name__ == '__main__':
     req = urllib.request.Request(sys.argv[1])
     try:
-        response = urllib.request.urlopen(req)
-        html = response.read()
-        print(html.decode("utf-8"))
+        with urllib.request.urlopen(req) as response:
+            html = response.read()
+            print(html.decode("utf-8"))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
