29d797faa2d13214850d33531e5f7c5a8acf1769
Kouam Franck <kouamfranck96@gmail.com>
2022-09-23 11:45:40 +0100

    0x11. Python - Network #1 | 2. POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This script takes in a URL and an email, sends a POST request to the passed
+   URL with the email as a parameter, and displays the body of the response
+   (decoded in utf-8)
+"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == '__main__':
+    values = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print(html)
