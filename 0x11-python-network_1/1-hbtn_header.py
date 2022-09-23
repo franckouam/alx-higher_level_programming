98a71c26c66accf895724a08d638a5c91375a70e
Kouam Franck <kouamfranck96@gmail.com>
2022-09-23 11:27:43 +0100

    0x11. Python - Network #1 | 1. Response header value #0 | Review

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,6 +6,7 @@
 import urllib.request
 import sys
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    html = response.info()
-    print(html.get("X-Request-Id"))
+if __name__ == '__main__':
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = response.info()
+        print(html.get("X-Request-Id"))
