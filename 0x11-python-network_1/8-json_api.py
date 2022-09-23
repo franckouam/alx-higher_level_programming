2027fce8d9aeb1e260e8224dcc52402bdd8aae2f
Kouam Franck <kouamfranck96@gmail.com>
2022-09-23 13:11:55 +0100

    0x11. Python - Network #1 | 8. Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This script takes in a letter and sends a POST request to
+   http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    q = ""
+    if len0(sys.argv) > 1:
+        q = sys.argv[1]
+    url = "http://0.0.0.0:5000/search_user"
+    data = {'q': q}
+    with requests.post(url, data=data) as response:
+        try:
+            j = response.json()
+            if j:
+                print("[{}] {}".format(j.get('id'), j.get('name')))
+            else:
+                print('No result')
+        except requests.exceptions.JSONDecodeError:
+            print("Not a valid JSON")
