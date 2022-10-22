edb2e018519fb25bd7d499c465438093fcabac40
Kouam Franck <kouamfranck96@gmail.com>
2022-10-22 02:46:10 +0100

    Cities and states

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -8,7 +8,7 @@ from model_state import Base, State
 from model_city import City
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@127.0.0.1:3306/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker()
